3dbfd53b4d1ba09cffd8492e2757011b283c7963
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2022-01-06 18:24:55 +0100

    usetypewritereffect

---
+++ components/useTypewriterEffect.js
@@ -0,0 +1,180 @@
+import React from "react";
+
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+export function Cursor({ typing, className }) {
+    const [toggler, setToggler] = React.useState(true);
+    React.useEffect(() => {
+        const id = setInterval(() => {
+            setToggler((prev) => !prev);
+        }, 500);
+
+        return () => {
+            clearInterval(id);
+        };
+    }, []);
+
+    return (
+        <span className={className}>{typing ? "|" : toggler ? "|" : ""}</span>
+    );
+}
+
+const initialState = { eventQueue: [], text: "" };
+
+const TW_ACTIONS = {
+    CONSUME: "consume",
+    SET_TEXT: "set_text",
+    APPEND_CHARACTER: "append_character",
+    DELETE_CHARACTER: "delete_character",
+};
+
+const TW_EVENT = {
+    TYPE_CHARACTER: "type_character",
+    DELETE_ALL: "delete_all",
+    DELETE_CHARACTER: "delete_some",
+    TYPE: "type",
+    PAUSE: "pause",
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case TW_EVENT.TYPE:
+            const events = action.payload
+                .split("")
+                .map((character) => ({
+                    event: TW_EVENT.TYPE_CHARACTER,
+                    value: character,
+                }));
+            return { ...state, eventQueue: [...state.eventQueue, ...events] };
+        case TW_EVENT.DELETE_CHARACTER: {
+            const events = [];
+            for (let i = 0; i < action.payload; ++i) {
+                events.push({
+                    event: TW_EVENT.DELETE_CHARACTER
+                })
+            }
+            return { ...state, eventQueue: [...state.eventQueue, ...events] };
+
+        }
+        case TW_EVENT.DELETE_ALL:
+            return {
+                ...state,
+                eventQueue: [
+                    ...state.eventQueue,
+                    { event: TW_EVENT.DELETE_ALL, value: null },
+                ],
+            };
+        case TW_ACTIONS.CONSUME: {
+            return {
+                ...state,
+                eventQueue: state.eventQueue.slice(1, state.eventQueue.length),
+            };
+        }
+        case TW_ACTIONS.SET_TEXT: {
+            return {
+                ...state,
+                text: action.payload
+            }
+        }
+        case TW_EVENT.PAUSE: {
+            return {
+                ...state,
+                eventQueue: [
+                    ...state.eventQueue,
+                    { event: TW_EVENT.PAUSE, value: action.payload },
+                ],
+            }
+        }
+        case TW_ACTIONS.APPEND_CHARACTER: {
+            return {
+                ...state,
+                text: state.text + action.payload
+            }
+        }
+        case TW_ACTIONS.DELETE_CHARACTER: {
+            return {
+                ...state,
+                text: state.text.slice(0, -1),
+            }
+        }
+        default:
+            throw new Error();
+    }
+}
+
+function useTypewriterEffect() {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+
+    const { eventQueue, text } = state;
+    React.useEffect(() => {
+        let lastFrame;
+        let pauseTime;
+        function step() {
+            if (!eventQueue.length) {
+                return;
+            }
+            raf = requestAnimationFrame(step);
+            const actualEvent = eventQueue[0];
+
+            if (!lastFrame) {
+                lastFrame = Date.now();
+            }
+            const now = Date.now();
+            const delta = now - lastFrame;
+            if (actualEvent.event === TW_EVENT.PAUSE) {
+                pauseTime = now.setSeconds(now.getSeconds() + actualEvent.value);
+                dispatch({ type: TW_ACTIONS.CONSUME });
+                return;
+            }
+            if(pauseTime != null) {
+                if(now < pauseTime) {
+                    return;
+                }
+                pauseTime = null;
+            }
+            const feelsNatural = getRandomArbitrary(60, 120);
+            if (delta < feelsNatural) {
+                return;
+            }
+            lastFrame = now;
+            if (actualEvent.event === TW_EVENT.TYPE_CHARACTER) {
+                dispatch({ type: TW_ACTIONS.APPEND_CHARACTER, payload: actualEvent.value })
+            }
+            else if (actualEvent.event === TW_EVENT.DELETE_ALL) {
+                dispatch({ type: TW_ACTIONS.SET_TEXT, payload: "" })
+            }
+            else if (actualEvent.event === TW_EVENT.DELETE_CHARACTER) {
+                dispatch({ type: TW_ACTIONS.DELETE_CHARACTER })
+            }
+            dispatch({ type: TW_ACTIONS.CONSUME });
+        }
+
+        let raf = requestAnimationFrame(step);
+
+        return () => cancelAnimationFrame(raf);
+    }, [eventQueue]);
+
+    return [text, dispatch, eventQueue.length !== 0];
+}
+
+export function type(dispatch, character) {
+    dispatch({ type: TW_EVENT.TYPE, payload: character });
+}
+
+export function deleteAll(dispatch) {
+    dispatch({ type: TW_EVENT.DELETE_ALL });
+}
+
+
+export function deleteSome(dispatch, number) {
+    dispatch({ type: TW_EVENT.DELETE_CHARACTER, payload: number });
+}
+
+export function pauseFor(dispatch, number) {
+    dispatch({ type: TW_EVENT.PAUSE, payload: number });
+}
+
+export default useTypewriterEffect;
---
+++ pages/temp.js
@@ -0,0 +1,28 @@
+import styles from "./index.module.css";
+import React from "react";
+import useTypewriterEffect, { Cursor, deleteAll, deleteSome, type } from "../components/useTypewriterEffect";
+import typewriterStyles from "../components/Typewriter.module.css";
+
+function temp() {
+    const [state, dispatch, isTyping] = useTypewriterEffect();
+    const [toType, setToType] = React.useState("");
+
+    React.useEffect(() => {
+        type(dispatch, "hello world!")
+        deleteSome(dispatch, 6)
+        type(dispatch, "universe!!")
+    }, [])
+
+    return (
+        <main className={styles.container}>
+            <input value={toType} onChange={(e) => setToType(e.target.value)} />
+            <button onClick={() => type(dispatch, toType)}>type</button>
+            <button onClick={() => deleteAll(dispatch)}>delete all</button>
+            <button onClick={() => deleteSome(dispatch, 5)}>delete some</button>
+
+            <p className={typewriterStyles.shell}>{state}<Cursor className={typewriterStyles.Cursor} typing={isTyping} /></p>
+        </main>
+    );
+}
+
+export default temp;
