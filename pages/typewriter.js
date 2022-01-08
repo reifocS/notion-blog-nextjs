e4571d1ef8f58e5506933ba9a8f1fec3f1e1c8dc
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2022-01-08 11:52:48 +0100

    add tw example

--- pages/typewriter.js
+++ pages/typewriter.js
@@ -1,47 +1,102 @@
 import styles from "./index.module.css";
-import React from "react";
+import React, { useRef } from "react";
 import useTypewriterEffect, {
-    getTypewriter,
-    useCursor,
+  getTypewriter,
+  useCursor,
 } from "use-typewriter-effect";
 import typewriterStyles from "../components/Typewriter.module.css";
 import Fable from "../components/Fable";
+import Prism from "prismjs";
+import "prismjs/components/prism-jsx.min";
+
+const metaCode = `function Code() {
+    const [code, dispatch] = useTypewriterEffect();
+  
+    React.useEffect(() => {
+      getTypewriter(dispatch).type(metaCode).trigger();
+    }, [dispatch]);
+  
+    return (
+      <pre className="language-jsx">
+        <code
+          dangerouslySetInnerHTML={{
+            __html: Prism.highlight(
+              code + "|",
+              Prism.languages["javascript"],
+              "javascript"
+            ),
+          }}
+          className='language-jsx'
+        />
+      </pre>
+    );
+  }`;
+function Code() {
+  const [code, dispatch] = useTypewriterEffect();
+
+  React.useEffect(() => {
+    getTypewriter(dispatch).type(metaCode).trigger();
+  }, [dispatch]);
+
+  return (
+    <pre className="language-jsx">
+      <code
+        dangerouslySetInnerHTML={{
+          __html: Prism.highlight(
+            code + "|",
+            Prism.languages["javascript"],
+            "javascript"
+          ),
+        }}
+        className={`language-jsx`}
+      />
+    </pre>
+  );
+}
 
 function Typewriter() {
-    const [state, dispatch, isTyping] = useTypewriterEffect();
-    const cursor = useCursor(isTyping);
+  const [state, dispatch, isTyping] = useTypewriterEffect();
+  const cursor = useCursor(isTyping);
 
-    React.useEffect(() => {
-        getTypewriter(dispatch)
-            .type("Hello world!\n How are you?")
-            .pauseFor(1000)
-            .deleteAll()
-            .type("This is a typewriter effect")
-            .pauseFor(1000)
-            .deleteSome(6)
-            .type("hook!")
-            .pauseFor(1000)
-            .deleteAll()
-            .setLoop(true)
-            .trigger()
-    }, [dispatch]);
+  React.useEffect(() => {
+    getTypewriter(dispatch)
+      .type("Hello world!\n How are you?")
+      .pauseFor(1000)
+      .deleteAll()
+      .type("This is a typewriter effect")
+      .pauseFor(1000)
+      .deleteSome(6)
+      .type("hook!")
+      .pauseFor(1000)
+      .deleteAll()
+      .setLoop(true)
+      .trigger();
+  }, [dispatch]);
 
-    const sentences = state.split("\n");
-    const lastSentence = sentences.pop();
-    return (
-        <main className={styles.container}>
-            <div className={typewriterStyles.shell}>
-                {sentences.map((sen, i) => (
-                    <p key={i}><span style={{color:"cyan"}}>$</span> {sen}</p>
-                ))}
-                <p>
-                    <span style={{color:"cyan"}}>$</span> {lastSentence}
-                    <span className={typewriterStyles.cursor} style={{visibility: cursor ? "visible" : "hidden"}}>|</span>
-                </p>
-            </div>
-            <Fable/>
-        </main>
-    );
+  const sentences = state.split("\n");
+  const lastSentence = sentences.pop();
+  return (
+    <main className={styles.container}>
+      <div className={typewriterStyles.shell}>
+        {sentences.map((sen, i) => (
+          <p key={i}>
+            <span style={{ color: "cyan" }}>$</span> {sen}
+          </p>
+        ))}
+        <p>
+          <span style={{ color: "cyan" }}>$</span> {lastSentence}
+          <span
+            className={typewriterStyles.cursor}
+            style={{ visibility: cursor ? "visible" : "hidden" }}
+          >
+            |
+          </span>
+        </p>
+      </div>
+      <Code />
+      <Fable />
+    </main>
+  );
 }
 
 export default Typewriter;
